2477b2685a4dcece4353c42963afeb7fbaed67dc
Jan Marais <maraisjandre9@gmail.com>
2024-10-24 08:33:17 +0200

    finaly got dummy output

--- src/whisper/imp.rs
+++ src/whisper/imp.rs
@@ -1,9 +1,10 @@
 use std::sync::LazyLock;
 
-use gst::glib;
+use byte_slice_cast::AsSliceOf;
 use gst::subclass::prelude::*;
+use gst::{glib, FlowError};
 use gst_audio::subclass::prelude::*;
-use gst_base::subclass::base_transform::GenerateOutputSuccess;
+use gst_base::subclass::base_transform::{GenerateOutputSuccess, PrepareOutputBufferSuccess};
 
 static CAT: LazyLock<gst::DebugCategory> = LazyLock::new(|| {
     gst::DebugCategory::new(
@@ -20,7 +21,6 @@ pub struct Whisper {}
 impl ObjectSubclass for Whisper {
     const NAME: &'static str = "Whisper";
     type Type = super::Whisper;
-    // type ParentType = gst_audio::AudioFilter;
     type ParentType = gst_base::BaseTransform;
 }
 
@@ -49,20 +49,20 @@ impl ElementImpl for Whisper {
 
     fn pad_templates() -> &'static [gst::PadTemplate] {
         static PAD_TEMPLATES: LazyLock<Vec<gst::PadTemplate>> = LazyLock::new(|| {
-            let caps = gst::Caps::builder("text/x-raw")
+            let src_caps = gst::Caps::builder("text/x-raw")
                 .field("format", "utf8")
                 .build();
             let src_pad_template = gst::PadTemplate::new(
                 "src",
                 gst::PadDirection::Src,
                 gst::PadPresence::Always,
-                &caps,
+                &src_caps,
             )
             .unwrap();
 
-            let caps = gst_audio::AudioCapsBuilder::new()
+            let sink_caps = gst_audio::AudioCapsBuilder::new()
                 .format(gst_audio::AUDIO_FORMAT_S16)
-                .layout(gst_audio::AudioLayout::NonInterleaved)
+                // .layout(gst_audio::AudioLayout::NonInterleaved)
                 .rate(16_000)
                 .channels(1)
                 .build();
@@ -70,14 +70,19 @@ impl ElementImpl for Whisper {
                 "sink",
                 gst::PadDirection::Sink,
                 gst::PadPresence::Always,
-                &caps,
+                &sink_caps,
             )
             .unwrap();
 
             vec![src_pad_template, sink_pad_template]
+            // vec![sink_pad_template, src_pad_template]
         });
         PAD_TEMPLATES.as_ref()
     }
+
+    // fn provide_clock(&self) -> Option<gst::Clock> {
+    //     Some(gst::SystemClock::obtain())
+    // }
 }
 
 impl BaseTransformImpl for Whisper {
@@ -86,12 +91,18 @@ impl BaseTransformImpl for Whisper {
     const PASSTHROUGH_ON_SAME_CAPS: bool = false;
     const TRANSFORM_IP_ON_PASSTHROUGH: bool = false;
 
+    fn start(&self) -> Result<(), gst::ErrorMessage> {
+        gst::info!(CAT, imp = self, "started ",);
+        Ok(())
+    }
+
     fn transform_caps(
         &self,
         direction: gst::PadDirection,
         _caps: &gst::Caps,
         filter: Option<&gst::Caps>,
     ) -> Option<gst::Caps> {
+        // gst::info!(CAT, imp = self, "tfm caps",);
         let mut caps = if direction == gst::PadDirection::Src {
             gst_audio::AudioCapsBuilder::new()
                 .format(gst_audio::AUDIO_FORMAT_S16)
@@ -113,21 +124,36 @@ impl BaseTransformImpl for Whisper {
     fn generate_output(
         &self,
     ) -> Result<gst_base::subclass::base_transform::GenerateOutputSuccess, gst::FlowError> {
-        if let Some(buffer) = self.take_queued_buffer() {
-            if buffer.flags().contains(gst::BufferFlags::DISCONT) {
-                self.drain()?;
-            }
-            gst::debug!(
-                CAT,
-                imp = self,
-                "received buffer of size {:?}",
-                buffer.size()
-            );
+        if let Some(b) = self.take_queued_buffer() {
+            let buffer_reader = b.as_ref().map_readable().map_err(|_| FlowError::Error)?;
+            let samples: &[i16] = buffer_reader.as_slice_of().map_err(|_| FlowError::Error)?;
+            gst::info!(CAT, imp = self, "yes output {}", samples.len());
+            let b = gst::Buffer::from_slice("hello".as_bytes());
+            Ok(GenerateOutputSuccess::Buffer(b))
         } else {
-            gst::debug!(CAT, imp = self, "no buffer in queue");
+            gst::info!(CAT, imp = self, "no output",);
+            Ok(GenerateOutputSuccess::NoOutput)
         }
-        Ok(GenerateOutputSuccess::NoOutput)
+        // gst::info!(CAT, imp = self, "generate",);
     }
+
+    // fn transform(
+    //     &self,
+    //     inbuf: &gst::Buffer,
+    //     outbuf: &mut gst::BufferRef,
+    // ) -> Result<gst::FlowSuccess, gst::FlowError> {
+    //     gst::info!(CAT, imp = self, "tfming",);
+    //     // Simulating output text for the example
+    //     let output_text = "Recognized speech text";
+
+    //     // Write the recognized text to the output buffer
+    //     let mut buffer_map = outbuf.map_writable().map_err(|_| gst::FlowError::Error)?;
+    //     buffer_map
+    //         .as_mut_slice()
+    //         .copy_from_slice(output_text.as_bytes());
+
+    //     Ok(gst::FlowSuccess::Ok)
+    // }
 
     // fn query(&self, direction: gst::PadDirection, query: &mut gst::QueryRef) -> bool {
 
